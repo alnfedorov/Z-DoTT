866cdba0e3ea10a9f7c7364e815669de291fad5a
alnfedorov <18511496+alnfedorov@users.noreply.github.com>
2025-09-27 13:03:42 +0100

    download-annotation.sh -> accept annotation versions as script parameters

--- setup/annotation/download-annotation.sh
+++ setup/annotation/download-annotation.sh
@@ -1,31 +1,14 @@
 #!/bin/bash
 set -euxo pipefail
 
-ASSEMBLIES=$1
-
-########################################################################################################################
-# GRCm39
-########################################################################################################################
-
-# GENCODE
-VERSION=M36
-
-wget -O "${ASSEMBLIES}/GRCm39/GRCm39.primary_assembly.genome.fa.gz" \
-  "https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_mouse/release_${VERSION}/GRCm39.primary_assembly.genome.fa.gz"
-
-wget -O "${ASSEMBLIES}/GRCm39/gencode/gencode.v${VERSION}.primary_assembly.annotation.gff3.gz" \
-  "https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_mouse/release_${VERSION}/gencode.v${VERSION}.primary_assembly.annotation.gff3.gz"
-
-wget -O "${ASSEMBLIES}/GRCm39/gencode/gencode.v${VERSION}.primary_assembly.annotation.gtf.gz" \
-  "https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_mouse/release_${VERSION}/gencode.v${VERSION}.primary_assembly.annotation.gtf.gz"
-
-# RefSeq
-wget -O "${ASSEMBLIES}/GRCm39/refseq/GRCm39.refseq.gff.gz" \
-  "https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/001/635/GCF_000001635.27_GRCm39/GCF_000001635.27_GRCm39_genomic.gff.gz"
+HUMAN_VERSION=$1
+MOUSE_VERSION=$2
+ASSEMBLIES=$3
 
 ########################################################################################################################
 # CHM13 v2.0
 ########################################################################################################################
+mkdir -p "${ASSEMBLIES}/CHM13v2"
 
 # Fasta
 wget -O "${ASSEMBLIES}/CHM13v2/CHM13v2.fa.gz" \
@@ -36,21 +19,38 @@ wget -O "${ASSEMBLIES}/CHM13v2/refseq/CHM13v2.refseq.gff.gz" \
   "https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/009/914/755/GCF_009914755.1_T2T-CHM13v2.0/GCF_009914755.1_T2T-CHM13v2.0_genomic.gff.gz"
 
 # GRCh38 GENCODE annotation and sequence for lift-over
-VERSION=47
 wget -O "${ASSEMBLIES}/CHM13v2/gencode/liftoff/GRCh38.primary_assembly.genome.fa.gz" \
-  "https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_${VERSION}/GRCh38.primary_assembly.genome.fa.gz"
+  "https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_${HUMAN_VERSION}/GRCh38.primary_assembly.genome.fa.gz"
 
-wget -O "${ASSEMBLIES}/CHM13v2/gencode/liftoff/gencode.v${VERSION}.primary_assembly.annotation.gff3.gz" \
-  "https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_${VERSION}/gencode.v${VERSION}.primary_assembly.annotation.gff3.gz"
+wget -O "${ASSEMBLIES}/CHM13v2/gencode/liftoff/gencode.v${HUMAN_VERSION}.primary_assembly.annotation.gff3.gz" \
+  "https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_${HUMAN_VERSION}/gencode.v${HUMAN_VERSION}.primary_assembly.annotation.gff3.gz"
+
+########################################################################################################################
+# GRCm39
+########################################################################################################################
+mkdir -p "${ASSEMBLIES}/GRCm39"
+
+wget -O "${ASSEMBLIES}/GRCm39/GRCm39.primary_assembly.genome.fa.gz" \
+  "https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_mouse/release_${MOUSE_VERSION}/GRCm39.primary_assembly.genome.fa.gz"
+
+wget -O "${ASSEMBLIES}/GRCm39/gencode/gencode.v${MOUSE_VERSION}.primary_assembly.annotation.gff3.gz" \
+  "https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_mouse/release_${MOUSE_VERSION}/gencode.v${MOUSE_VERSION}.primary_assembly.annotation.gff3.gz"
+
+wget -O "${ASSEMBLIES}/GRCm39/gencode/gencode.v${MOUSE_VERSION}.primary_assembly.annotation.gtf.gz" \
+  "https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_mouse/release_${MOUSE_VERSION}/gencode.v${MOUSE_VERSION}.primary_assembly.annotation.gtf.gz"
+
+# RefSeq
+wget -O "${ASSEMBLIES}/GRCm39/refseq/GRCm39.refseq.gff.gz" \
+  "https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/001/635/GCF_000001635.27_GRCm39/GCF_000001635.27_GRCm39_genomic.gff.gz"
 
 ########################################################################################################################
 # Postprocessing
 ########################################################################################################################
 
-for file in ${ASSEMBLIES}/*/*.fa.gz
-do
-  gunzip "${file}"
-  bgzip "${file/.fa.gz/.fa}"
-  echo "Indexing" ${file}
-  samtools faidx "${file}"
+for file in "${ASSEMBLIES}"/*/*.fa.gz; do
+  saveto="${file/.fa.gz/.fa.bgz}"
+  gunzip "${file}" --stdout | bgzip --threads "$(nproc)" -l 6 -o "${saveto}" /dev/stdin
+  rm "${file}"
+  echo "Indexing" "${saveto}"
+  samtools faidx "${saveto}"
 done
