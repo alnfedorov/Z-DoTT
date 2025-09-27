c0eef3fe47a0c3e9212dd457e1140133dc0d987e
alnfedorov <18511496+alnfedorov@users.noreply.github.com>
2025-09-27 13:17:34 +0100

    download-annotation.sh -> rely on commited refseq annotation

--- setup/annotation/download-annotation.sh
+++ setup/annotation/download-annotation.sh
@@ -14,9 +14,9 @@ mkdir -p "${ASSEMBLIES}/CHM13v2"
 wget -O "${ASSEMBLIES}/CHM13v2/CHM13v2.fa.gz" \
   "https://s3-us-west-2.amazonaws.com/human-pangenomics/T2T/CHM13/assemblies/analysis_set/chm13v2.0.fa.gz"
 
-# RefSeq
-wget -O "${ASSEMBLIES}/CHM13v2/refseq/CHM13v2.refseq.gff.gz" \
-  "https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/009/914/755/GCF_009914755.1_T2T-CHM13v2.0/GCF_009914755.1_T2T-CHM13v2.0_genomic.gff.gz"
+## RefSeq
+#wget -O "${ASSEMBLIES}/CHM13v2/refseq/CHM13v2.refseq.gff.gz" \
+#  "https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/009/914/755/GCF_009914755.1_T2T-CHM13v2.0/GCF_009914755.1_T2T-CHM13v2.0_genomic.gff.gz"
 
 # GRCh38 GENCODE annotation and sequence for lift-over
 wget -O "${ASSEMBLIES}/CHM13v2/gencode/liftoff/GRCh38.primary_assembly.genome.fa.gz" \
@@ -39,9 +39,9 @@ wget -O "${ASSEMBLIES}/GRCm39/gencode/gencode.v${MOUSE_VERSION}.primary_assembly
 wget -O "${ASSEMBLIES}/GRCm39/gencode/gencode.v${MOUSE_VERSION}.primary_assembly.annotation.gtf.gz" \
   "https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_mouse/release_${MOUSE_VERSION}/gencode.v${MOUSE_VERSION}.primary_assembly.annotation.gtf.gz"
 
-# RefSeq
-wget -O "${ASSEMBLIES}/GRCm39/refseq/GRCm39.refseq.gff.gz" \
-  "https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/001/635/GCF_000001635.27_GRCm39/GCF_000001635.27_GRCm39_genomic.gff.gz"
+## RefSeq
+#wget -O "${ASSEMBLIES}/GRCm39/refseq/GRCm39.refseq.gff.gz" \
+#  "https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/001/635/GCF_000001635.27_GRCm39/GCF_000001635.27_GRCm39_genomic.gff.gz"
 
 ########################################################################################################################
 # Postprocessing
