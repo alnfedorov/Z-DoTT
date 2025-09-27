2d1b6a9e0efb6321f95ff91b1665ea282291f0dc
alnfedorov <18511496+alnfedorov@users.noreply.github.com>
2025-09-27 18:58:12 +0100

    WIP

--- data/PRJNA256013/seq-project.py
+++ data/PRJNA256013/seq-project.py
@@ -50,7 +50,7 @@ for exp in project.experiments:
     exp.attributes["title"] = title
 
 object.__setattr__(
-    project, "description", "4sU/Total RNA-seq of Human Foreskin Fibroblasts (HFF) infected with HSV-1"
+    project, "description", "Time-course 4sU/Total RNA-seq of Human Foreskin Fibroblasts (HFF) infected with HSV-1"
 )
 
 seqproj.adapter.yaml.dump(project, FOLDER / "seq-project.yaml")
