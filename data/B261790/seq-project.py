9460b8c09b19e78428c668c59dc2bb1170aeec95
alnfedorov <18511496+alnfedorov@users.noreply.github.com>
2025-10-08 20:41:06 +0100

    B261790

--- data/B261790/seq-project.py
+++ data/B261790/seq-project.py
@@ -1,108 +1,74 @@
+from collections import defaultdict
 from pathlib import Path
 
-import pandas as pd
-from biobit.toolkit import seqproj
+from biobit.toolkit import nfcore, seqproj
 
-from lib.seqproj import JCCSeq
+FOLDER = Path(__file__).parent
 
-ROOT = Path(__file__).parent
-FASTQ = ROOT / "fastq"
+FASTQ = FOLDER / "fastq"
 
-
-def sample_builder(ind: str, data: pd.DataFrame) -> seqproj.Sample:
-    tags = set(data["Tags"])
-    if len(tags) == 2:
-        assert len(tags) == 2 and all(x[0] == "April2022" and x[1] == "HSV" for x in tags), tags
-        subind1, subind2 = tags.pop()[-1], tags.pop()[-1]
-        assert ind == f"{subind1}+{subind2}" or ind == f"{subind2}+{subind1}", (ind, subind1, subind2)
-    else:
-        assert len(tags) == 1
-        subind1, subind2 = ind.split("+")
-        assert tags.pop()[-1] in {subind1, subind2}, (ind, subind1, subind2)
-
-    attributes = {"cells": "MEF"}
-    if {subind1, subind2}.issubset({"A1", "B1", "C1", "G1", "H1", "A2", "E2", "F2", "G2", "C3", "D3", "E3"}):
-        attributes["treatment"] = "mock"
-        organism = {"Mus musculus"}
-    elif {subind1, subind2}.issubset({"D1", "H2", "E1", "A3", "F1", "B3", "B2", "F3", "C2", "G3", "D2", "H3"}):
-        attributes["treatment"] = "HSV-1"
-        attributes["time-point"] = "8h"
-        attributes["HSV-1"] = "F strain[VR-733]"
-        organism = {"Mus musculus", "Herpes simplex virus 1"}
-    else:
-        raise ValueError(f"Unknown plates: {(ind, subind1, subind2)}")
-
-    attributes["replica"] = {
-        "A1+E2": "1", "B1+F2": "1", "C1+G2": "1", "D1+H2": "1", "E1+A3": "1", "F1+B3": "1", "G1+C3": "2", "H1+D3": "2",
-        "A2+E3": "2", "B2+F3": "2", "C2+G3": "2", "D2+H3": "2",
-    }[ind]
-
-    return seqproj.Sample(ind=ind, organism=organism, attributes=attributes)
-
-
-def library_builder(_: str, __: str, data: pd.DataFrame) -> seqproj.Library:
-    tags = set(data['Tags'])
-    assert len(tags) == 1
-    _, _, plate = tags.pop()
-
-    if plate in {"E2", "F2", "G2", "H2", "A3", "B3", "C3", "D3", "E3", "F3", "G3", "H3"}:
-        # Input tags
-        selection = {"Total RNA", "rRNA depletion"}
-    elif plate in {"A1", "D1", "G1", "B2"}:
-        # IgG tags
-        selection = {"Total RNA", "rRNA depletion", "IgG RIP"}
-    elif plate in {"B1", "E1", "H1", "C2"}:
-        selection = {"Total RNA", "rRNA depletion", "Z22 RIP"}
-    else:
-        raise ValueError(f"Unknown plate: {plate}")
-
-    return seqproj.Library({"RNA"}, selection, seqproj.Strandedness.Reverse)
-
-
-def experiment_builder(
-        ind: str, sample: seqproj.Sample, library: seqproj.Library, runs: tuple[seqproj.Run, ...], _: pd.DataFrame
-) -> seqproj.Experiment:
-    if "Z22 RIP" in library.selection:
-        selection = "Z22-RIP"
-    elif "IgG RIP" in library.selection:
-        selection = "IgG-RIP"
-    else:
-        assert library.selection == {"Total RNA", "rRNA depletion"}
-        selection = "input"
-
-    title = f"{sample.attributes['cells']}_{sample.attributes['treatment']}_{selection}_{sample.attributes['replica']}"
-    return seqproj.Experiment(ind=ind, sample=sample, library=library, runs=runs, attributes={"title": title})
-
-
-def project_builder(
-        inds: tuple[str, ...], experiments: tuple[seqproj.Experiment, ...], samples: tuple[seqproj.Sample, ...]
-):
-    assert len(inds) == 1
-    return seqproj.Project(f"MEF HSV-1 batch 1 [{ROOT.name}, {inds[0]}]", experiments, samples)
-
-
-data = JCCSeq.initialize(FASTQ.glob("**/*.fastq.gz"), ROOT)
-
-# Remap input & RIP samples
-remapping = {
-    "A1": "A1+E2", "B1": "B1+F2", "D1": "D1+H2", "E1": "E1+A3", "G1": "G1+C3",
-    "H1": "H1+D3", "B2": "B2+F3", "C2": "C2+G3", "E2": "A1+E2", "F2": "B1+F2",
-    "G2": "C1+G2", "H2": "D1+H2", "A3": "E1+A3", "B3": "F1+B3", "C3": "G1+C3", "D3": "H1+D3", "E3": "A2+E3",
-    "F3": "B2+F3", "G3": "C2+G3", "H3": "D2+H3"
-}
-data["Lane"] = [f"{s}-{l}" for s, l in data[["Sample", "Lane"]].itertuples(index=False)]
-data["Sample"] = data["Tags"].apply(lambda x: x[2]).map(remapping)
-assert data.isnull().sum().sum() == 0, data.isnull().sum()
-
-project = JCCSeq.parse(
-    data,
-    seq_machine="Illumina NovaSeq 6000",
-    sample_builder=sample_builder,
-    library_builder=library_builder,
-    experiment_builder=experiment_builder,
-    project_builder=project_builder
+loaded = nfcore.fetchngs.load_seqproj(
+    samplesheet=FASTQ / "samplesheet.csv",
+    fastq_root=Path("fastq")
 )
+assert len(loaded) == 1, f"Expected 1 seqproject, got {len(loaded)} for {FOLDER.name}"
+project = loaded[0]
 
+# Recreate the samples and experiments structure
+samples = defaultdict(list)
+for exp in project.experiments:
+    mef, condition, selection, postfix = exp.sample.attributes.pop('title').split(', ')
+    assert mef == "MEF"
+    assert condition in {"mock", "HSV-1"}
+    assert selection in {"IgG", "Z22", "IgG-input", "Z22-input", "RIP-input"}
+    assert postfix.endswith(f" [{FOLDER.name}]")
+    replica = postfix.split(' ')[0]
+    assert replica in {"1", "2"}
+
+    exp.sample.attributes["cells"] = "MEF"
+    exp.sample.attributes["treatment"] = condition
+    exp.sample.attributes["replica"] = replica
+
+    ip = selection.replace("-input", "")
+    samples[condition, ip, replica].append(exp)
+
+    object.__setattr__(exp.library, "source", {"RNA"})
+    object.__setattr__(exp.library, "strandedness", seqproj.Strandedness.Reverse)
+    if selection in {"IgG", "Z22"}:
+        object.__setattr__(exp.library, "selection", {"rRNA depletion", f"{selection} RIP"})
+        exp.library.attributes["RIP"] = selection
+    else:
+        assert selection.endswith("-input")
+        object.__setattr__(exp.library, "selection", {"rRNA depletion"})
+        exp.library.attributes["RIP"] = "input"
+
+    title = f"MEF_{condition}_{selection}_{replica}"
+    exp.attributes['title'] = title
+
+# Merge IP and input experiments into one biological sample to better reflect the experimental design
+new_samples = []
+for (condition, ip, replica), exps in samples.items():
+    # Keep extra RIP inputs as is
+    if ip == "RIP":
+        assert len(exps) == 1
+        new_samples.append(exps[0].sample)
+        continue
+
+    # Otherwise, we expect exactly one IP and one input experiment per sample
+    assert len(exps) == 2, f"Expected 2 experiment per sample, got {len(exps)} for {(condition, ip, replica)}"
+    ip, inp = exps[0].sample, exps[1].sample
+    assert ip.attributes == inp.attributes
+    assert ip.organism == inp.organism
+
+    sample = seqproj.Sample(
+        ind=f"{ip.ind}-{inp.ind}", organism=ip.organism, attributes=ip.attributes
+    )
+    object.__setattr__(exps[0], "sample", sample)
+    object.__setattr__(exps[1], "sample", sample)
+    new_samples.append(sample)
+
+object.__setattr__(project, "samples", new_samples)
+object.__setattr__(project, "ind", FOLDER.name)
 object.__setattr__(project, "description", "Z22 RIP-seq of HSV-1 infected MEFs (batch 1)")
 
-seqproj.adapter.yaml.dump(project, ROOT / "seq-project.yaml")
+seqproj.adapter.yaml.dump(project, FOLDER / "seq-project.yaml")
